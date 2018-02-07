390653cec34233d9c6b3582f51a914df7f3a6bb9
Alexey Ramazanov <me@sharkus.net>
2018-02-07 17:43:47 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,32 +14,31 @@ ActiveRecord::Schema.define(version: 20160521114434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                           null: false
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.string   "remember_me_token"
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.string   "activation_state"
-    t.string   "users"
-    t.string   "activation_token"
+    t.string "activation_state"
+    t.string "users"
+    t.string "activation_token"
     t.datetime "activation_token_expires_at"
-    t.string   "reset_password_token"
+    t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
-    t.string   "last_login_from_ip_address"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.index ["activation_token"], name: "index_users_on_activation_token", using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
+    t.string "last_login_from_ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activation_token"], name: "index_users_on_activation_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
