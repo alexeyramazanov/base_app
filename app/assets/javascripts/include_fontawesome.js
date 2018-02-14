b8172e570ce16c9db1fd5ac46448b1766faa1dc8
Alexey Ramazanov <me@sharkus.net>
2018-02-14 12:56:01 +0300

    Font Awesome 5

---
+++ app/assets/javascripts/include_fontawesome.js
@@ -0,0 +1,4 @@
+//= require @fortawesome/fontawesome/index
+//= require @fortawesome/fontawesome-free-regular/index
+//= require @fortawesome/fontawesome-free-solid/index
+//= require @fortawesome/fontawesome-free-brands/index
