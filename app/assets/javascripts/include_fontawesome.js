f7eedcaa4ae8c0e249097364df63f2cc55261e3d
Alexey Ramazanov <me@sharkus.net>
2018-12-31 17:16:16 +0300

    finalize rails 5.2 upgrade

--- app/assets/javascripts/include_fontawesome.js
+++
@@ -1,4 +0,0 @@
-//= require @fortawesome/fontawesome/index
-//= require @fortawesome/fontawesome-free-regular/index
-//= require @fortawesome/fontawesome-free-solid/index
-//= require @fortawesome/fontawesome-free-brands/index
