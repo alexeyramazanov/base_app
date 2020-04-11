65196f32a34f8fc2a8fafd84bfec3a4498659b5d
Alexey Ramazanov <me@sharkus.net>
2020-04-11 14:23:45 +0300

    Stop using sprockets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,7 +48,5 @@ Rails.application.configure do
   # Raises error for missing translations.
   config.action_view.raise_on_missing_translations = true
 
-  config.assets.raise_runtime_errors = true
-
   config.active_job.queue_adapter = :inline
 end
