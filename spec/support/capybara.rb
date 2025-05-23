29aa5060ec4ab965911fc4b3cfcd240229bd8f49
Alexey Ramazanov <me@sharkus.net>
2025-05-23 11:53:10 +0300

    Add capybara-screenshot gem (#46)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1920,1080')
+  options.add_argument('--force-device-scale-factor=1')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 
 Capybara.server = :puma, { Silent: true }
 Capybara.default_driver = :selenium_chrome_headless # use :selenium_chrome for debugging
