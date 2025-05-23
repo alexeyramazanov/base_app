6e608d71c2e08a04edddb3b218ef3e7b05148e18
Alexey Ramazanov <me@sharkus.net>
2025-05-23 12:26:55 +0300

    Attempt to fix flaky capybara spec

--- spec/features/admin/api_tokens_spec.rb
+++ spec/features/admin/api_tokens_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Admin::ApiTokensController do
       visit admin_api_tokens_path
 
       select user.email, from: 'User'
-      sleep 0.2
+      sleep 0.5
       click_button 'Create'
 
       within('#modal') do
