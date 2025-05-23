0776a75ac3502a0b102ecea23946680de1eb627c
Alexey Ramazanov <me@sharkus.net>
2025-05-23 14:11:01 +0300

    Show browser logs in console when capybara spec fails (#48)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -9,9 +9,23 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options.add_argument('--disable-gpu')
   options.add_argument('--window-size=1920,1080')
   options.add_argument('--force-device-scale-factor=1')
+  options.logging_prefs = { 'browser' => 'ALL' }
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.server = :puma, { Silent: true }
 Capybara.default_driver = :selenium_chrome_headless # use :selenium_chrome for debugging
+
+RSpec.configure do |config|
+  config.after(:each, type: :feature) do |example|
+    next unless example.exception
+
+    errors = page.driver.browser.logs.get(:browser)
+
+    if errors.present?
+      message = errors.map(&:message).join("\n")
+      puts message
+    end
+  end
+end
