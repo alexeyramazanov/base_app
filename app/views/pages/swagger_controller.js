b6bfc97426dd9beeb97e48a6dcd8b06046edb06b
Alexey Ramazanov <me@sharkus.net>
2026-04-30 16:56:13 +0300

    Vite 8, Uppy 5, FontAwesome 7, etc. (#104)

--- app/views/pages/swagger_controller.js
+++ app/views/pages/swagger_controller.js
@@ -1,9 +1,9 @@
 import {Controller} from '@hotwired/stimulus';
-import SwaggerUI from 'swagger-ui';
+import { SwaggerUIBundle } from 'swagger-ui-dist';
 
 export default class extends Controller {
   connect() {
-    SwaggerUI({
+    SwaggerUIBundle({
       url: window.location.origin + '/public_api/swagger_doc.json',
       dom_id: '#swagger',
       deepLinking: true,
